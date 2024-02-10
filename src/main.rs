95e001544e37b92e4435544c02a3815a714a3aef
Taylor <taylor@irreducible.io>
2024-02-09 20:10:03 -0700

    Parse prisma schema

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::path::PathBuf;
+use std::{
+    io::{BufReader, Read},
+    path::PathBuf,
+};
 
 use clap::Parser;
+use psl::parse_schema;
 
 /// Visualize a Prisma schema as a d2 diagram.
 #[derive(Debug, Parser)]
@@ -16,7 +20,25 @@ struct Args {
     output_file: Option<PathBuf>,
 }
 
-fn main() {
+fn input_reader(path: Option<PathBuf>) -> Result<Box<dyn Read>, std::io::Error> {
+    if let Some(path) = path {
+        let f = std::fs::File::open(path)?;
+        Ok(Box::new(BufReader::new(f)))
+    } else {
+        Ok(Box::new(std::io::stdin()))
+    }
+}
+
+fn read_input(read: &mut dyn Read) -> Result<String, std::io::Error> {
+    let mut s = String::new();
+    read.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    println!("{:?}", args);
+    let input = read_input(&mut input_reader(args.input_file)?)?;
+    let parsed = parse_schema(&input)?;
+    println!("{:?}", parsed);
+    Ok(())
 }
